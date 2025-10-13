5358d3baf2856c9f03a62b3e967019745595da66
Kauan <kauandeon.dev@gmail.com>
2025-10-13 14:54:39 -0300

    Método 'POST' funcionando direto no BD

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
 const users = []
 
-app.post('/usuarios', (req,res) =>{
-    users.push(req.body)
+app.post('/usuarios', async(req,res) =>{
+
+
+    await prisma.user.create({
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    
     res.status(201).json(req.body)
 })
 app.get('/usuarios', (req, res) => {
