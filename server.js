acda6a71d7e8ad413f49469596121f1938dcd305
Kauan <kauandeon.dev@gmail.com>
2025-10-13 11:54:32 -0300

    Banco de dados integrado

--- server.js
+++ server.js
@@ -1,57 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-
+const users = []
 
 app.post('/usuarios', (req,res) =>{
-    res.send("Sasukee")
+    users.push(req.body)
+    res.status(201).json(req.body)
 })
 app.get('/usuarios', (req, res) => {
-    res.send("Narutooo")
+    res.status(200).json(users)
 })
 
 app.listen(3000)
-// import express from 'express'
-// import { PrismaClient } from '../node_modules/@prisma/client/default.js'
-// const prisma = new PrismaClient()
-// const app = express() //agora dentro do app a gente tem acesso a todo o express
-
-// /*
-//     Objetivo: Criar uma API de Usuários
-//     Passos:
-//     - Criar um usuário
-//     - Listar todos os usuários
-//     - Editar um usuário
-//     - Deletar um usuário
-// */
-
-
-
-// app.use(express.json())
-// const users = []
-// app.post('/usuarios', async (req, res) => {
-
-//     await prisma.User.create({
-//         data: {
-//             email: req.body.email,
-//             name: req.body.name,
-//             age: req.body.age
-//         }
-//     })
-//     res.status(201).json(req.body)
-// })
-
-// app.get('/usuarios', (req, res) => { // req, res = request, response
-//     res.status(200).json(users)               // requisição e resposta (a ponte para o front e back)
-// })
-
-// app.listen(3000)
-// /*
-//     As rotas(get,push,put...) precisam :
-//     1)Tipo de Rota / Método HTTP
-//     2)Endereço. Ex: www.importacao.com/usuarios <-- Esse último
-// */
 
 
 
