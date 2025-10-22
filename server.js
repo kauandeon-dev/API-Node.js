762588166bdb810c46c11d445bddbdd4d4e0a1e2
Kauan <kauandeon.dev@gmail.com>
2025-10-22 15:22:22 -0300

    API Finalizada

--- server.js
+++ server.js
@@ -21,11 +21,48 @@ app.post('/usuarios', async(req,res) =>{
     res.status(201).json(req.body)
 })
 app.get('/usuarios', async(req, res) => {
-
-    const users = await prisma.user.findMany()
+    let users = []
+    if (req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: parseInt(req.query.age)
+            }
+        })
+    }else{
+        users = await prisma.user.findMany()
+    }
 
     res.status(200).json(users)
 })
+
+app.put('/usuarios/:id', async(req,res) =>{
+
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    
+    res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) =>{
+    await prisma.user.delete({
+        where: {
+            id: req.params.id,
+        },
+    })
+        res.status(200).json({ message: "Usuário deletado com sucesso"})
+})
+
 
 app.listen(3000)
 
